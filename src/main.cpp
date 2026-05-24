db094a07b65825826fc3ece9d49b54e9ddae4cda
Ivo Blok <ivoblokdoorn@gmail.com>
2026-05-24 20:48:11 +0200

    added renderer code back in, added physical device selection

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,15 @@
 #include <renderer/renderer.hpp>
 
 int main() {
-    RenderEngine engine;
-    std::cout << "Hello World!\n";
+    RenderEngine renderer;
+
+    try {
+        renderer.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
--- src/renderer/include/renderer/renderer.hpp
+++ src/renderer/include/renderer/renderer.hpp
@@ -1,12 +1,48 @@
 #pragma once
 
-#include <common_types/command_queue.hpp>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vector>
+#include <optional>
+#include <iostream>
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value();
+    }
+};
+
 
 class RenderEngine {
 public:
-    RenderEngine(int a = 0);
-    ~RenderEngine();
+    void run();
 
 private:
+    GLFWwindow* window;
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
+    void initWindow();
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+
+    void createInstance();
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+    void setupDebugMessenger();
+    void pickPhysicalDevice();
+    bool isDeviceSuitable(VkPhysicalDevice device);
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    std::vector<const char*> getRequiredExtensions();
+    bool checkValidationLayerSupport();
+    
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
+    }
 };

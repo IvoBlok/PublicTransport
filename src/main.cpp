0930816ed7f1fce70afaf5b2581da8277bc5cb35
Ivo Blok <ivoblokdoorn@gmail.com>
2026-05-24 14:17:23 +0200

    got a better idea of how meson works, created empty window with glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,62 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
-#include <vector>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    uint32_t layerCount;
-    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
-    std::vector<VkLayerProperties> availableLayers(layerCount);
-    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
-    std::cout << "Available Vulkan layers: " << layerCount << std::endl;
-    for (const auto& layer : availableLayers) {
-        std::cout << "  - " << layer.layerName << std::endl;
+
+class HelloTriangeApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    return 0;
+private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+
+int main() {
+    HelloTriangeApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
+

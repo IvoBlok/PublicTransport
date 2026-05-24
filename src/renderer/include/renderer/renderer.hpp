c045d394bd579efd0dba48f4b19d472c76d745a9
Ivo Blok <ivoblokdoorn@gmail.com>
2026-05-24 20:10:07 +0200

    added general project structure, with common_types, renderer and compute split up. Added meson files to handle the dependencies and compilation process (library + executable)

---
+++ src/renderer/include/renderer/renderer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <common_types/command_queue.hpp>
+
+class RenderEngine {
+public:
+    RenderEngine(int a = 0);
+    ~RenderEngine();
+
+private:
+
+};
---
+++ src/renderer/src/renderer.cpp
@@ -0,0 +1,10 @@
+#include <renderer/renderer.hpp>
+#include <iostream>
+
+RenderEngine::RenderEngine(int a) {
+    std::cout << "hello from engine! " << a << "\n";
+}
+
+RenderEngine::~RenderEngine() {
+    
+}
